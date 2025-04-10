580529b4234270169732eff5b8cf96dd0826b7c0
RUTURAJ1989 <115629673+RUTURAJ1989@users.noreply.github.com>
2025-04-10 06:12:48 +0000

    fbapi

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: "AIzaSyCdimEpxhfYQkmaJuWUjhegu227c-rhfY0",
-        authDomain: "ipl-auction-f62cf.firebaseapp.com",
-        databaseURL: "https://ipl-auction-f62cf-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "ipl-auction-f62cf",
-        storageBucket: "ipl-auction-f62cf.appspot.com",
-        messagingSenderId: "1006195476972",
-        appId: "1:1006195476972:web:6343541fb0007925ded8c9"
+        const firebaseConfig = {
+            apiKey: process.env.APIKEY,
+            authDomain: process.env.AUTHDOMAIN,
+            databaseURL: process.env.DATABASEURL,
+            projectId: process.env.PROJECTID,
+            storageBucket: process.env.STORAGEBUCKET,
+            messagingSenderId: process.env.MESSAGINGSENDER_ID,
+            appId: process.env.APPID
+          };
     });
 }
 
