76098a10a507e04965332b9dfe4b640e58341dec
RUTURAJ1989 <115629673+RUTURAJ1989@users.noreply.github.com>
2025-04-10 09:10:06 +0000

    fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 if (!firebase.apps.length) {
-    firebase.initializeApp({
-        const firebaseConfig = {
-            apiKey: process.env.apiKey,
-            authDomain: process.env.authDomain,
-            databaseURL: process.env.databaseURL,
-            projectId: process.env.projectId,
-            storageBucket: process.env.storageBucket,
-            messagingSenderId: process.env.messagingSenderId,
-            appId: process.env.appId
-          };
-    });
+    const firebaseConfig = {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        databaseURL: process.env.databaseURL,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId
+    };
+
+    firebase.initializeApp(firebaseConfig);
 }
 
 export const db = firebase.firestore();
